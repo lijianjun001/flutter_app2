d695be765a362f6dc65bf32ea3ecccfe26ae5655
lijianjun <1455404626@qq.com>
2019-01-08 16:27:23 +0800

    添加bottomNavigation

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _tabIndex = 0;
   var tabImages;
   var appBarTitles = ['首页', '商铺', '消息', '我的'];
+  var _bodys = [new HomePage(), new ShopPage(), new MsgPage(), new MyPage()];
 
-  /*
-   * 根据image路径获取图片
-   * 这个图片的路径需要在 pubspec.yaml 中去定义
-   */
   Image getTabImage(path) {
     return new Image.asset(path, width: 20.0, height: 20.0);
   }
@@ -93,28 +90,24 @@ class _MyHomePageState extends State<MyHomePage> {
      */
     tabImages = [
       [
-        getTabImage('images/main_farm_icon_select.png'),
-        getTabImage('images/main_farm_icon.png')
+        getTabImage('images/main_find_icon.png'),
+        getTabImage('images/main_find_icon_select.png')
+
       ],
       [
-        getTabImage('images/main_farm_icon_select.png'),
-        getTabImage('images/main_farm_icon.png')
+        getTabImage('images/main_find_icon.png'),
+        getTabImage('images/main_find_icon_select.png')
       ],
       [
-        getTabImage('images/main_farm_icon_select.png'),
-        getTabImage('images/main_farm_icon.png')
+        getTabImage('images/main_find_icon.png'),
+        getTabImage('images/main_find_icon_select.png')
       ],
       [
-        getTabImage('images/main_farm_icon_select.png'),
-        getTabImage('images/main_farm_icon.png')
+        getTabImage('images/main_find_icon.png'),
+        getTabImage('images/main_find_icon_select.png')
       ]
     ];
 
-    /*
-   * 存储的四个页面，和Fragment一样
-   */
-    var _bodys;
-    _bodys = [new HomePage(), new ShopPage(), new MsgPage(), new MyPage()];
   }
 
   void _incrementCounter() {
@@ -142,36 +135,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: _bodys[_tabIndex],
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
